3f120ce10c6b86dbdb1d67c8c97a58f8ee40d1b8
Paul Grever <paulgrever@gmail.com>
2015-06-14 12:19:01 -0600

    rpsec testing passing

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -8,4 +8,14 @@ RSpec.describe Job, type: :model do
     expect(@job.title).to eq("Title 1")
     expect(@job.job_description).to eq("Description")
   end
+
+  it "must have a title" do
+    job = Job.new(job_description: "test")
+    expect(job).to_not be_valid
+  end
+
+  it "must have a job_description" do
+    job = Job.new(title: "test")
+    expect(job).to_not be_valid
+  end
 end
