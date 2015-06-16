b8d59ba1907c5d6d1d88b13fc3dfcaf7e2d806cd
Valentino Espinoza <xvalentino13@gmail.com>
2015-06-16 13:47:26 -0600

    hound fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ActiveRecord::Base
   def self.find_or_create_by_auth(auth_data)
-    user = self.find_or_create_by_uid(auth_data["uid"])
+    user = find_or_create_by_uid(auth_data["uid"])
     user.name = auth_data["info"]["name"]
     user.image = auth_data["info"]["image"]
     user.email = auth_data["info"]["email"]
     user.save
-    return user
+    user
   end
 
   def self.find_or_create_by_uid(uid)
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,10 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it "has attributes" do
-    user = User.find_or_create_by_auth({'info' => {uid: 1, name: "fake name", image: "fakeurl.com/image.gif", email: 'example@gmail.com'}})
+    User.find_or_create_by_auth({'info' => {uid: 1, 
+                                            name: "fake name", 
+                                            image: "fakeurl.com/image.gif", 
+                                            email: 'example@gmail.com'}})
     expect(User.count).to eq(1)
   end
 end
