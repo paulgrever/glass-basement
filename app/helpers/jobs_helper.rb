df0742e03f1fa90b6df7830340a405bb5a84e96a
Paul Grever <paulgrever@gmail.com>
2015-06-18 14:19:48 -0600

    broken test - check to see if this is issue with my redis

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -2,12 +2,4 @@ module JobsHelper
   def company
     @job.company.name
   end
-
-  # def new_company
-  #   @job.company.name
-  # end
-
-  # def new_region
-  #   @job.company.region.place
-  # end
 end
--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,4 @@
 require 'redis'
 
+
 $redis = Redis.new
