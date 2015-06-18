d7204e1544249b751d587e512433ea93d2929a8a
Valentino Espinoza <xvalentino13@gmail.com>
2015-06-18 15:24:15 -0600

    this might work?

--- app/assets/javascripts/chats.js
+++ app/assets/javascripts/chats.js
@@ -1,8 +1,16 @@
 $(document).ready(function(){
-  $("#chat-form").on("ajax:success", function(e, data, status, xhr){
+  var appendMessage =$("#chat-form").on("ajax:success", function(e, data, status, xhr){
     $(".messages").append("<div>" + data.body + " </div>");
     return $(" #chat-input" ).val("");
   }).on("ajax:error", function(e, xhr, status, error){
     console.log(error);
   });
 });
+
+var socket = io('http://localhost:2222');
+
+socket.on(chatId, function(channel, message) {
+  appendMessage(_, message);
+}
+
+}
