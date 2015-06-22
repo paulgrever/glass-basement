51b9ba5e824278e18d3bf8671997d2674cd15d79
Paul Grever <paulgrever@gmail.com>
2015-06-21 22:42:52 -0600

    hound fixes rd 2

--- spec/models/interview_spec.rb
+++ spec/models/interview_spec.rb
@@ -1,22 +1,25 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Interview, type: :model do
-  before(:each) do 
+  before(:each) do
     @company = Company.create(name: "Company 1")
     @job = Job.create(title: "Title 1",
                       job_description: "Description",
                       company_id: @company.id)
-    @user = User.create(uid: "123455", name: "Paul Grever", email: "paulgrever@gmail.com", image: "https://avatars.githubusercontent.com/u/3664281?v=3")
+    @user = User.create(uid: "123455",
+                        name: "Paul Grever",
+                        email: "paulgrever@gmail.com",
+                        image: "https://avatars.githubusercontent.com/u/3664281?v=3")
     @interview = @job.interviews.create(title: "Interview 1",
                                         number: 1,
                                         interview_type: "phone",
-                                        details: "Met with John Smith on the phone, he was nice",
+                                        details: "Met with John Smith on the phone",
                                         date: "2014-09-20",
                                         time_between: "2 weeks",
                                         questions: "Question 1: How was your day?",
                                         interviewer_name: "Paul Grever",
                                         interviewer_email: "paulgrever@gmail.com",
-                                        interviewer_phone: "608-239-2471", 
+                                        interviewer_phone: "608-239-2471",
                                         user_id: @user.id)
   end
 
@@ -36,13 +39,13 @@ RSpec.describe Interview, type: :model do
     interview = Interview.new(title: "Interview 1",
                               number: 1,
                               interview_type: "phone",
-                              details: "Met with John Smith on the phone, he was nice",
+                              details: "Met with John Smith on the phone",
                               date: "2014-09-20",
                               time_between: "2 weeks",
                               questions: "Question 1: How was your day?",
                               interviewer_name: "Paul Grever",
                               interviewer_email: "paulgrever@gmail.com",
-                              interviewer_phone: "608-239-2471", 
+                              interviewer_phone: "608-239-2471",
                               user_id: @user.id)
     expect(interview).to_not be_valid
   end
@@ -52,13 +55,13 @@ RSpec.describe Interview, type: :model do
                               job_id: @job.id,
                               number: 1,
                               interview_type: "phone",
-                              details: "Met with John Smith on the phone, he was nice",
+                              details: "Met with John Smith on the phone",
                               date: "2014-09-20",
                               time_between: "2 weeks",
                               questions: "Question 1: How was your day?",
                               interviewer_name: "Paul Grever",
                               interviewer_email: "paulgrever@gmail.com",
-                              interviewer_phone: "608-239-2471", 
+                              interviewer_phone: "608-239-2471",
                               user_id: nil)
     expect(interview).to_not be_valid
   end
@@ -68,13 +71,13 @@ RSpec.describe Interview, type: :model do
                               job_id: @job.id,
                               number: 1,
                               interview_type: "phone",
-                              details: "Met with John Smith on the phone, he was nice",
+                              details: "Met with John Smith on the phone",
                               date: "2014-09-20",
                               time_between: "2 weeks",
                               questions: "Question 1: How was your day?",
                               interviewer_name: "Paul Grever",
                               interviewer_email: "paulgrever@gmail.com",
-                              interviewer_phone: "608-239-2471", 
+                              interviewer_phone: "608-239-2471",
                               user_id: @user.id)
     expect(interview).to_not be_valid
   end
